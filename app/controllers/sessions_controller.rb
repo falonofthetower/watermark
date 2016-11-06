bbbee4b96b6e504b052cf001b3aaca8f528b85cf
Peter Karth <falonofthetower@gmail.com>
2016-11-06 00:30:19 -0400

    Switches it to using omniauth for logins
    - using only google at this point

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,31 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:success] = "Welcome to Iolite"
-      redirect_to new_watermark_path
-    else
-      flash[:danger] = "Your sign in failed!"
-      redirect_to sign_in_path
-    end
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_path
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You have been signed out!"
-    redirect_to sign_in_path
+    redirect_to root_path
   end
+
+  # def create
+  #   user = User.find_by(email: params[:email])
+  #   if user && user.authenticate(params[:password])
+  #     session[:user_id] = user.id
+  #     flash[:success] = "Welcome to Iolite"
+  #     redirect_to new_watermark_path
+  #   else
+  #     flash[:danger] = "Your sign in failed!"
+  #     redirect_to sign_in_path
+  #   end
+  # end
+
+  # def destroy
+  #   session[:user_id] = nil
+  #   flash[:success] = "You have been signed out!"
+  #   redirect_to sign_in_path
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105171915) do
+ActiveRecord::Schema.define(version: 20161106032957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,11 +71,16 @@ ActiveRecord::Schema.define(version: 20161105171915) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "access_token"
-    t.string "refresh_token"
-    t.string "password_digest"
-    t.string "email"
-    t.string "google_drive_id"
+    t.string   "access_token"
+    t.string   "refresh_token"
+    t.string   "password_digest"
+    t.string   "email"
+    t.string   "google_drive_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
   create_table "watermarks", force: :cascade do |t|
